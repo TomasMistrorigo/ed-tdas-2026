d586e97f9e9ab415d6982be4bc86a22283d200bb
Tomas Mistrorigo <tomasmistrorigo04@gmail.com>
2026-04-03 16:06:20 -0300

    modified:   .classpath 	new file:   src/tps/tp3/ejercicio4/ColaConPila.java 	deleted:    tp3/ejercicio4/ColaConPila.java

---
+++ src/tps/tp3/ejercicio4/ColaConPila.java
@@ -0,0 +1,21 @@
+
+import ar.edu.uns.cs.ed.tdas.tdacola.Queue;
+import java.util.Stack;
+
+public class ColaConPila<E> implements Queue<E>{
+    private int cant;
+    private E[] colaPila;
+
+    public ColaConPila(){
+        cant=0;
+        colaPila=(E[]) new Object[20];
+    }
+
+    @Override
+    public int size(){return cant;}
+
+}
+
+//Programe una clase llamada ColaConPila que implemente la interfaz Queue<E> provista por la cátedra utilizando una
+//pila (java.util.Stack). Ejecute el test JUnit correspondiente para verificar que la funcionalidad de la clase sea correcta.
+//Utilice la interface Queue<E> y el test distribuido por la cátedra.
