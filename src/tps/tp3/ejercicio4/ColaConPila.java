f8ec2e67dd1b3af5b255550c7ea3df2a8814e055
Tomas Mistrorigo <tomasmistrorigo04@gmail.com>
2026-04-11 19:30:51 -0300

    modified:   src/test/java/ar/edu/uns/cs/ed/tdas/tdacola/QueueTest.java 	modified:   src/tps/tp3/ejercicio4/ColaConPila.java

--- src/tps/tp3/ejercicio4/ColaConPila.java
+++ src/tps/tp3/ejercicio4/ColaConPila.java
@@ -1,35 +1,66 @@
+package tp3.ejercicio4;
 
 import ar.edu.uns.cs.ed.tdas.tdacola.Queue;
 import java.util.Stack;
+import ar.edu.uns.cs.ed.tdas.excepciones.EmptyQueueException;;
 
 public class ColaConPila<E> implements Queue<E>{
-    private int cant;
-    private E[] colaPila;
+    private Stack<E> ingreso;
+    private Stack<E> salida;
 
     public ColaConPila(){
-        cant=0;
-        colaPila=(E[]) new Object[20];
+        ingreso = new Stack<>();
+        salida = new Stack<>();
     }
 
     @Override
-    public int size(){return cant;}
+    public int size(){
+        int cont=0;
+        while(!ingreso.empty()){
+            salida.push(ingreso.pop());
+            cont++;
+        }
+        while(!salida.empty())
+            ingreso.push(salida.pop());
+        return cont;
+    }
 
     @Override
-    public boolean isEmpty(){return cant==0;}
+    public boolean isEmpty(){
+        return ingreso.empty();
+    }
 
     @Override
     public E front(){
-
+        if(ingreso.empty())
+            throw new EmptyQueueException("Cola Vacia");
+        E rta=null;
+        while(!ingreso.empty()){
+            salida.push(ingreso.pop());
+        }
+        rta=salida.peek();
+        while(!salida.empty())
+            ingreso.push(salida.pop());
+        return rta;
     }
 
     @Override
     public void enqueue(E element){
-
+        ingreso.push(element);
     }
 
     @Override
     public E dequeue(){
-        
+        if(ingreso.empty())
+            throw new EmptyQueueException("Cola Vacia");
+        E rta=null;
+        while(!ingreso.empty()){
+            salida.push(ingreso.pop());
+        }
+        rta=salida.pop();
+        while(!salida.empty())
+            ingreso.push(salida.pop());
+        return rta;        
     }
 }
 
